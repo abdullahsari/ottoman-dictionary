e1cfa179ade33100a351a72619321d9cb57ff5ca
Muhammed Sari <muhammed.abdullah.sari@hotmail.com>
2018-03-10 00:01:28 +0100

    ⭐️ Application shell (#1)
    * Add book svg
    
    * Initial appication shell structure
    
    * should be using sidenav instead of drawer
    
    * Use correct prop binding syntax
    
    * Change breakpoint to SM instead of XS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,39 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ServiceWorkerModule } from '@angular/service-worker';
+
+import { LayoutModule } from '@angular/cdk/layout';
+import {
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatToolbarModule,
+} from '@angular/material';
 
 import { environment } from '../environments/environment';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './components/app/app.component';
 
+const materials = [
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatToolbarModule,
+];
+
 @NgModule({
     declarations: [AppComponent],
     imports: [
         AppRoutingModule,
+        BrowserAnimationsModule,
         BrowserModule,
+        LayoutModule,
         ServiceWorkerModule.register('/ngsw-worker.js', {
             enabled: environment.production,
         }),
+        ...materials,
     ],
     providers: [],
     bootstrap: [AppComponent],
