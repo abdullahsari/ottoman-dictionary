bb83a9b19c9394ceb44696735c0de729833bf5f6
Muhammed Sari <muhammed.abdullah.sari@hotmail.com>
2018-03-10 00:01:28 +0100

    ⭐️ Application shell (#1)
    * Add book svg
    
    * Initial appication shell structure
    
    * should be using sidenav instead of drawer
    
    * Use correct prop binding syntax
    
    * Change breakpoint to SM instead of XS

--- src/app/components/app/app.component.ts
+++ src/app/components/app/app.component.ts
@@ -1,14 +1,28 @@
-import { Component } from '@angular/core';
+import { MediaMatcher } from '@angular/cdk/layout';
+import { ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-    public title: string;
+export class AppComponent implements OnDestroy, OnInit {
+    private _mobileQueryListener: () => void;
+    public mobileQuery: MediaQueryList;
 
-    constructor() {
-        this.title = 'personal dictionary';
+    constructor(
+        private _changeDetectorRef: ChangeDetectorRef,
+        private _mediaMatcher: MediaMatcher
+    ) {}
+
+    public ngOnDestroy(): void {
+        this.mobileQuery.removeListener(this._mobileQueryListener);
+    }
+
+    public ngOnInit(): void {
+        this.mobileQuery = this._mediaMatcher.matchMedia('(min-width: 961px)');
+        this._mobileQueryListener = () =>
+            this._changeDetectorRef.detectChanges();
+        this.mobileQuery.addListener(this._mobileQueryListener);
     }
 }
