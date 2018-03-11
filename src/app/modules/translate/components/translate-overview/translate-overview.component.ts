7a23b4baca6c472abb8bb063336b43232dfeb675
Muhammed Sari <muhammed.abdullah.sari@hotmail.com>
2018-03-11 20:26:38 +0100

    First version of translation page

--- src/app/modules/translate/components/translate-overview/translate-overview.component.ts
+++ src/app/modules/translate/components/translate-overview/translate-overview.component.ts
@@ -1,12 +1,115 @@
-import { Component, OnInit } from '@angular/core';
+import { animate, style, transition, trigger } from '@angular/animations';
+import {
+    AfterViewInit,
+    Component,
+    ElementRef,
+    OnDestroy,
+    OnInit,
+    ViewChild,
+} from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
+import { ErrorObservable } from 'rxjs/observable/ErrorObservable';
+import { fromEvent } from 'rxjs/observable/fromEvent';
+import { of } from 'rxjs/observable/of';
+import { catchError } from 'rxjs/operators/catchError';
+import { debounceTime } from 'rxjs/operators/debounceTime';
+import { distinctUntilChanged } from 'rxjs/operators/distinctUntilChanged';
+import { filter } from 'rxjs/operators/filter';
+import { pluck } from 'rxjs/operators/pluck';
+import { switchMap } from 'rxjs/operators/switchMap';
 
+import { Translation } from '../../../../shared/models/translation.interface';
+import { SnackbarService } from '../../../../shared/services/snackbar.service';
+import { OttomanService } from '../../services/ottoman.service';
+
+/**
+ * The overview component for translating
+ * @author Muhammed Sari <hi@muhammedsari.me>
+ */
 @Component({
     selector: 'app-translate-overview',
     templateUrl: './translate-overview.component.html',
     styleUrls: ['./translate-overview.component.scss'],
+    animations: [
+        trigger('popAnimation', [
+            transition(':enter', [
+                style({ transform: 'scale(0)', opacity: 0 }),
+                animate(
+                    '.2s ease-out',
+                    style({ transform: 'scale(1)', opacity: 1 })
+                ),
+            ]),
+            transition(':leave', [
+                animate(
+                    '.2s ease-in',
+                    style({ transform: 'scale(0)', opacity: 0 })
+                ),
+            ]),
+        ]),
+    ],
 })
-export class TranslateOverviewComponent implements OnInit {
-    constructor() {}
+export class TranslateOverviewComponent
+    implements AfterViewInit, OnDestroy, OnInit {
+    private _subscription: Subscription;
+    public isTranslating: boolean;
+    public translation: Translation;
+    @ViewChild('input') private _textarea: ElementRef;
+
+    constructor(
+        private _ottomanService: OttomanService,
+        private _snackbarService: SnackbarService
+    ) {}
+
+    public ngAfterViewInit(): void {
+        this._subscription = fromEvent(this._textarea.nativeElement, 'input')
+            .pipe(
+                distinctUntilChanged(),
+                debounceTime(500),
+                pluck('srcElement', 'value'),
+                filter(word => {
+                    const bool = word !== '';
+                    if (!bool) this.translation = null;
+                    return bool;
+                }),
+                switchMap((word: string) => {
+                    this.isTranslating = true;
+                    return this._ottomanService.translate(word);
+                }),
+                catchError(err => {
+                    if (err instanceof TypeError) {
+                        return of({
+                            searched: 'Could not translate',
+                            results: [],
+                        });
+                    }
+                    return ErrorObservable.create(err);
+                })
+            )
+            .subscribe(
+                (t: Translation) => {
+                    this.translation = t;
+                    this.isTranslating = false;
+                },
+                err => {
+                    this._snackbarService.notify(
+                        'Error while attempting the translation.'
+                    );
+                }
+            );
+    }
+
+    public ngOnDestroy(): void {
+        this._subscription.unsubscribe();
+    }
 
     public ngOnInit(): void {}
+
+    /**
+     * Clears the textarea content
+     */
+    public clearInput(): void {
+        this._textarea.nativeElement.value = '';
+        this._textarea.nativeElement.focus();
+        this.translation = null;
+    }
 }
--- src/app/modules/translate/translate.module.ts
+++ src/app/modules/translate/translate.module.ts
@@ -1,12 +1,31 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import {
+    MatButtonModule,
+    MatCardModule,
+    MatDividerModule,
+    MatIconModule,
+    MatListModule,
+    MatProgressBarModule,
+} from '@angular/material';
+
+import { SharedModule } from '../../shared/shared.module';
 
 import { TranslateOverviewComponent } from './components/translate-overview/translate-overview.component';
 import { OttomanService } from './services/ottoman.service';
 import { TranslateRoutingModule } from './translate-routing.module';
 
+const materials = [
+    MatButtonModule,
+    MatCardModule,
+    MatDividerModule,
+    MatIconModule,
+    MatListModule,
+    MatProgressBarModule,
+];
+
 @NgModule({
-    imports: [CommonModule, TranslateRoutingModule],
+    imports: [CommonModule, SharedModule, TranslateRoutingModule, ...materials],
     declarations: [TranslateOverviewComponent],
     providers: [OttomanService],
 })
