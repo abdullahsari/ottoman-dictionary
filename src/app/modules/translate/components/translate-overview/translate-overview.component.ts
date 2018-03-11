d99ce90e4c191269fdc55aab06b5a3c95d93ecbe
Muhammed Sari <muhammed.abdullah.sari@hotmail.com>
2018-03-11 15:26:15 +0100

    Created TranslateOverviewComponent

---
+++ src/app/modules/translate/components/translate-overview/translate-overview.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-translate-overview',
+    templateUrl: './translate-overview.component.html',
+    styleUrls: ['./translate-overview.component.scss'],
+})
+export class TranslateOverviewComponent implements OnInit {
+    constructor() {}
+
+    public ngOnInit(): void {}
+}
