9b1feebb97b965982a21806bae3f8009ed947cc6
Muhammed Sari <muhammed.abdullah.sari@hotmail.com>
2018-03-14 23:53:30 +0100

    Created and implemented AnonymousGuard, which does the reverse job of the AuthGuard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { LoginComponent } from './components/login/login.component';
+import { AnonymousGuard } from './guards/anonymous.guard';
 import { AuthGuard } from './guards/auth.guard';
 
 export const declarations = [LoginComponent];
-export const providers = [AuthGuard];
+export const providers = [AuthGuard, AnonymousGuard];
 
 const routes: Routes = [
     {
         path: 'login',
+        canActivate: [AnonymousGuard],
         component: LoginComponent,
     },
     {
