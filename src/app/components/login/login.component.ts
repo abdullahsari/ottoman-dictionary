8ee198495136039bce7376f19f4f9f7f0318735d
Muhammed Sari <muhammed.abdullah.sari@hotmail.com>
2018-03-17 23:30:01 +0100

    Add ottoman anthem as a bonus to login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -16,13 +16,16 @@ import { SocialType } from '../../shared/models/social-type.enum';
     styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements AfterViewInit {
+    private _anthem: HTMLAudioElement;
     public listening: boolean;
 
     constructor(
         private _authService: AuthService,
         private _domEventsService: DOMEventsService,
         private _router: Router
-    ) {}
+    ) {
+        this._anthem = new Audio();
+    }
 
     public ngAfterViewInit(): void {
         this._domEventsService.triggerOnDocument(APP_READY_EVENT);
@@ -49,6 +52,14 @@ export class LoginComponent implements AfterViewInit {
      * Either plays the Ottoman anthem or mutes it
      */
     public toggleAnthem(): void {
+        if (this.listening) {
+            this._anthem.pause();
+        } else {
+            this._anthem.src = '../../assets/audio/anthem.mp3';
+            this._anthem.load();
+            this._anthem.currentTime = 0;
+            this._anthem.play();
+        }
         this.listening = !this.listening;
     }
 }
