8f3bc0a80467d4049c72ee0fda129b8d0594ce6b
Muhammed Sari <muhammed.abdullah.sari@hotmail.com>
2018-03-21 21:10:44 +0100

    Inject Web Speech API

--- src/app/modules/translate/translate.module.ts
+++ src/app/modules/translate/translate.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 
+import { webSpeechFactory } from '../core/factories/web-speech.factory';
 import { SharedModule } from '../shared/shared.module';
 
 import { TranslateOverviewComponent } from './components/translate-overview/translate-overview.component';
 import { OttomanService } from './services/ottoman.service';
+import { SpeechService } from './services/speech.service';
 import { TranslateRoutingModule } from './translate-routing.module';
 
 @NgModule({
     imports: [SharedModule, TranslateRoutingModule],
     declarations: [TranslateOverviewComponent],
-    providers: [OttomanService],
+    providers: [
+        OttomanService,
+        { provide: 'speech', useFactory: webSpeechFactory },
+        SpeechService,
+    ],
 })
 export class TranslateModule {}
