f8bd95bcd34a72a2aa6b1f9d451b851b49710a42
Muhammed Sari <muhammed.abdullah.sari@hotmail.com>
2018-03-24 00:43:12 +0100

    UsersOverview initial layout and styling to display users

--- src/app/modules/users/components/users-overview/users-overview.component.ts
+++ src/app/modules/users/components/users-overview/users-overview.component.ts
@@ -1,14 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
 
+import { User } from '../../../../common/models/user.interface';
 import { PageTitleService } from '../../../core/services/page-title.service';
+import { UsersService } from '../../services/users.service';
 
 @Component({
     selector: 'app-users-overview',
     templateUrl: './users-overview.component.html',
     styleUrls: ['./users-overview.component.scss'],
 })
-export class UsersOverviewComponent {
-    constructor(private _pageTitleService: PageTitleService) {
+export class UsersOverviewComponent implements OnInit {
+    public users$: Observable<User[]>;
+
+    constructor(
+        private _pageTitleService: PageTitleService,
+        private _usersService: UsersService
+    ) {
         _pageTitleService.title = 'Users';
+    }
+
+    public ngOnInit(): void {
+        this.users$ = this._usersService.fetchAll();
     }
 }
