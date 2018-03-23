3ba751bf49024dc616fd0de5d930b08729876ecb
Muhammed Sari <muhammed.abdullah.sari@hotmail.com>
2018-03-24 00:40:36 +0100

    Created UserStatusComponent to display a user's status (initial)

--- src/app/modules/users/users.module.ts
+++ src/app/modules/users/users.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 
 import { SharedModule } from '../shared/shared.module';
 
+import { UserStatusComponent } from './components/user-status/user-status.component';
 import { UsersRoutingModule, declarations } from './users-routing.module';
 
 @NgModule({
     imports: [SharedModule, UsersRoutingModule],
-    declarations,
+    declarations: [UserStatusComponent, ...declarations],
 })
 export class UsersModule {}
